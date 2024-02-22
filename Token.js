6c7669cb8151a5179ef47b61a8d60601878e7e3f
wdcs-asheeskumar <154244737+wdcs-asheeskumar@users.noreply.github.com>
2024-02-22 13:48:27 +0530

    Update Token.js

--- Token.js
+++ Token.js
@@ -5,7 +5,6 @@
 
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
-
 /// @dev "describe" block is imported from Mocha framework and it is used to define a test suite.
 /// @dev "describe" takes two parameters, first a string for test suite and second a function for perfoming task
 /// @dev "it" also takes two parameters first a string for defining the test case and then a funtion for performing task
@@ -14,24 +13,17 @@ const { ethers } = require("hardhat");
 describe("Token", function () {
     it("Deployment should assign the total supply to the owner", async function () {
         const [owner] = await ethers.getSigners(); /// @notice assigning account to the owner
-        // console.log("Owner",owner);
         const Token = await ethers.getContractFactory("Token"); /// @notice creating an instance of the contract
         const hardhatToken = await Token.deploy(); /// @notice deployment of contract
         const ownerBalance = await hardhatToken.balanceOf(owner.address); /// @notice checking balance of the contract by calling balanceOf function through the instance of contract
-        // console.log("Owner balance:", ownerBalance);
-        // console.log("Owner Address:", owner.address);
         const result = expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
-        // console.log("result of testing", result);
     });
 
     it("Transfer token from owner address to another", async () => {
         const [owner, transferAddress] = await ethers.getSigners();
-        // console.log("owner address, transfer address", owner, transferAddress);
-
         const Token = await ethers.getContractFactory("Token");
         const hardhatToken = await Token.deploy();
         const tranferToken = await hardhatToken.transfer(transferAddress.address, 10);
         const result = expect(await hardhatToken.balanceOf(transferAddress)).to.equal(10);
-        // console.log("result:", result);
     });
 });
