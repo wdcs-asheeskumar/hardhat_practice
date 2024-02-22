7f19376fcc450ad46996694a84deac84f6f4ef44
wdcs-asheeskumar <154244737+wdcs-asheeskumar@users.noreply.github.com>
2024-02-22 13:44:32 +0530

    Update Token.js

--- Token.js
+++ Token.js
@@ -1,13 +1,23 @@
+/// @title Testing of Token contract  
+/// @author Ashees Kumar
+/// @notice This contract performs the testing for the Token contract 
+/// @dev Chai and Mocha frameworks are used for testing and ethers are imported through Hardhat
+
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
+/// @dev "describe" block is imported from Mocha framework and it is used to define a test suite.
+/// @dev "describe" takes two parameters, first a string for test suite and second a function for perfoming task
+/// @dev "it" also takes two parameters first a string for defining the test case and then a funtion for performing task
+/// @dev There can be multiple "it" block within one "describe" block
+/// @dev "describe" and "it" blocks are part of Mocha framework and they are useful for organizing and structuring the test cases 
 describe("Token", function () {
     it("Deployment should assign the total supply to the owner", async function () {
-        const [owner] = await ethers.getSigners(); // assigning account to the owner
+        const [owner] = await ethers.getSigners(); /// @notice assigning account to the owner
         // console.log("Owner",owner);
-        const Token = await ethers.getContractFactory("Token"); // creating an instance of the contract
-        const hardhatToken = await Token.deploy(); // deployment of contract
-        const ownerBalance = await hardhatToken.balanceOf(owner.address); // checking balance of the contract by calling balanceOf function through the instance of contract
+        const Token = await ethers.getContractFactory("Token"); /// @notice creating an instance of the contract
+        const hardhatToken = await Token.deploy(); /// @notice deployment of contract
+        const ownerBalance = await hardhatToken.balanceOf(owner.address); /// @notice checking balance of the contract by calling balanceOf function through the instance of contract
         // console.log("Owner balance:", ownerBalance);
         // console.log("Owner Address:", owner.address);
         const result = expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
@@ -25,19 +35,3 @@ describe("Token", function () {
         // console.log("result:", result);
     });
 });
-
-
-// Testing
-/*
-   describe() and it() blocks are used to test the condition which is the part of "Mocha" framework
-   It helps in organising the test cases and checking them in order in which we want
-   It also helps in writing the test cases in a more readable way
-   descrive() : Here first we have to write a "test suite" and  then a function in which we will be performing operations.
-   it() : This function has a string descrbing the function and then a function to perform operations
-   there can be multiple it() in describe() function.
-   expect() : This function is used to check wether the desired result is achieved or not
-   First assign the address to the owner 
-   Then create an instance of the Token contract
-   Then by using the instance of the contract we can call any function that is the part of that contract
-   Then we can check wether the desired result is achieved or not using expect().
-*/
